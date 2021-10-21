85856d2c222e892299427ed77687d9b5cc1db9f9
Maxime <ma.vandromme@laposte.net>
2021-10-21 14:15:36 +0200

    Fin du projet

---
+++ Frontend/src/App.vue
@@ -0,0 +1,26 @@
+<!-- fichier central de l'qpplication Vue.-->
+
+<template>
+    <div id="app">
+      <Navi></Navi>
+      <router-view></router-view>
+    </div>
+</template>
+
+<script>
+import Navi from "./components/Navi";
+
+export default {
+    name: "App",
+    components: {
+      Navi
+    }
+}
+</script>
+
+<style>
+  body {
+    background: rgb(255, 0, 0);
+    background: linear-gradient(62deg, #ff5353 0%, #ffc6ec 100%);
+  }
+</style>
